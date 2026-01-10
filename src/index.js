e58bd1bb2563f8f66cd4fc0b960f6bcf5d3eea9b
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-01-10 16:59:09 +0200

    refactor Sphere

--- src/index.js
+++ src/index.js
@@ -14,7 +14,12 @@ const camera = new Camera(new Vector3())
 const scene = new Scene()
 const rayTracer = new RayTracer(camera, scene, viewport.distanceToCamera, 100)
 
-const sphere1 = new Sphere()
+const sphere1 = new Sphere(new Vector3(0, -1, 3), 1, new Color(255, 0, 0))
+const sphere2 = new Sphere(new Vector3(2, 0, 4), 1, new Color(0, 0, 255))
+const sphere3 = new Sphere(new Vector3(-0, -1, 3), 1, new Color(0, 255, 0))
+scene.add(sphere1)
+scene.add(sphere2)
+scene.add(sphere3)
 
 canvas.clear()
 
--- src/object/Sphere.js
+++ src/object/Sphere.js
@@ -1,12 +1,21 @@
+import Color from "../math/Color.js"
+import Vector3 from "../math/Vector3.js"
 import Object3d from "./Object3d.js"
 
 export default class Sphere extends Object3d {
     /** @private @type{number} */ #radius
+    /** @private @type{Color} */ #color
 
-    constructor(position, radius) {
+    /**
+    * @param {Vector3} position
+    * @param {number} radius - Must be positive.
+    * @param {Color} color
+    */
+    constructor(position, radius, color) {
         super(position)
 
         this.radius = radius
+        this.color = color
     }
 
     get radius() {
@@ -15,6 +24,16 @@ export default class Sphere extends Object3d {
 
     set radius(radius) {
         if (typeof radius !== 'number') throw new TypeError("Parameter 'radius' is not number")
+        if (radius <= 0) throw new RangeError("Parameter 'radius' is not positive")
         this.#radius = radius
+    }
+
+    get color() {
+        return this.#color
+    }
+
+    set color(color) {
+        if (!(color instanceof Color)) throw new TypeError("Parameter 'color' is not Color")
+        this.#color = color
     }
 }
