489d48aa6c6c22f74bb5e2d5437a507dcf29d2b5
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-02-10 20:43:05 +0200

    comment out raytracer setup code

--- src/index.js
+++ src/index.js
@@ -12,57 +12,57 @@ import Viewport from "./Viewport.js"
 
 const [width, height] = [window.innerWidth, window.innerHeight]
 
-const viewport = new Viewport({ width: 1, height: height / width }, 1)
-const camera = new Camera({ position: new Vector3(3, 0, 1) })
-camera.rotateY(-45)
+// const viewport = new Viewport({ width: 1, height: height / width }, 1)
+// const camera = new Camera({ position: new Vector3(3, 0, 1) })
+// camera.rotateY(-45)
 
-const sphere1 = new Sphere({
-    position: new Vector3(0, -1, 3),
-    radius: 1,
-    color: new Color(255, 0, 0),
-    specular: 200,
-    reflective: 0.2,
-})
-const sphere2 = new Sphere({
-    position: new Vector3(2, 0, 4),
-    radius: 1,
-    color: new Color(0, 0, 255),
-    specular: 500,
-    reflective: 0.3
-})
-const sphere3 = new Sphere({
-    position: new Vector3(-2, 0, 4),
-    radius: 1,
-    color: new Color(0, 255, 0),
-    specular: 0,
-    reflective: 0.4
-})
-const sphere4 = new Sphere({
-    position: new Vector3(0, -5001, 0),
-    radius: 5000,
-    color: new Color(255, 255, 0),
-    specular: 1000,
-    reflective: 0.5
-})
-
-const ambientLight = new AmbientLight({ intensity: 0.2 })
-const pointLight = new PointLight({
-    position: new Vector3(2, 1, 0),
-    intensity: 0.6
-})
-const directionalLight = new DirectionalLight({
-    direction: new Vector3(1, 4, 4),
-    intensity: 0.2
-})
-
-const scene = new Scene()
-scene.add(sphere1)
-scene.add(sphere2)
-scene.add(sphere3)
-scene.add(sphere4)
-scene.add(ambientLight)
-scene.add(pointLight)
-scene.add(directionalLight)
+// const sphere1 = new Sphere({
+//     position: new Vector3(0, -1, 3),
+//     radius: 1,
+//     color: new Color(255, 0, 0),
+//     specular: 200,
+//     reflective: 0.2,
+// })
+// const sphere2 = new Sphere({
+//     position: new Vector3(2, 0, 4),
+//     radius: 1,
+//     color: new Color(0, 0, 255),
+//     specular: 500,
+//     reflective: 0.3
+// })
+// const sphere3 = new Sphere({
+//     position: new Vector3(-2, 0, 4),
+//     radius: 1,
+//     color: new Color(0, 255, 0),
+//     specular: 0,
+//     reflective: 0.4
+// })
+// const sphere4 = new Sphere({
+//     position: new Vector3(0, -5001, 0),
+//     radius: 5000,
+//     color: new Color(255, 255, 0),
+//     specular: 1000,
+//     reflective: 0.5
+// })
+//
+// const ambientLight = new AmbientLight({ intensity: 0.2 })
+// const pointLight = new PointLight({
+//     position: new Vector3(2, 1, 0),
+//     intensity: 0.6
+// })
+// const directionalLight = new DirectionalLight({
+//     direction: new Vector3(1, 4, 4),
+//     intensity: 0.2
+// })
+//
+// const scene = new Scene()
+// scene.add(sphere1)
+// scene.add(sphere2)
+// scene.add(sphere3)
+// scene.add(sphere4)
+// scene.add(ambientLight)
+// scene.add(pointLight)
+// scene.add(directionalLight)
 
 const canvas = new Canvas('#canvas', {
     width,
