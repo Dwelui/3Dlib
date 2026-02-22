3841c411c42af6c6b32669d4ead5e47580a6a6ed
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-02-22 14:36:45 +0200

    add vite

--- test/unit/first.test.js
+++ test/unit/first.test.js
@@ -1,4 +1,13 @@
 import { expect, test } from 'vitest'
+import Color from '../../src/Color.js'
+
+test('Color defaults', () => {
+    const color = new Color()
+    expect(color.r).toBe(0)
+    expect(color.g).toBe(0)
+    expect(color.b).toBe(0)
+    expect(color.a).toBe(255)
+})
 
 test('adds 1 + 2 to equal 3', () => {
     expect(2 + 2).toBe(4)
