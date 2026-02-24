fa29079a9ee9510c01cb5e5788b066036a95728a
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-02-24 20:54:49 +0200

    polish tests

--- test/unit/math/matrix4.test.js
+++ test/unit/math/matrix4.test.js
@@ -1,36 +1,38 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import Matrix3 from "../../../src/math/Matrix3.js"
 import Matrix4 from "../../../src/math/Matrix4.js";
 import Vector3 from "../../../src/math/Vector3.js";
 
-test('from matrix3', () => {
-    const m3 = new Matrix3([
-        1, 2, 3,
-        4, 5, 6,
-        7, 8, 9
-    ])
+describe.concurrent('from', () => {
+    test('matrix3', () => {
+        const m3 = new Matrix3([
+            1, 2, 3,
+            4, 5, 6,
+            7, 8, 9
+        ])
 
-    const m4 = Matrix4.fromMatrix3(m3)
+        const m4 = Matrix4.fromMatrix3(m3)
 
-    expect(m4.toArray()).toStrictEqual([
-        m3.get(0), m3.get(1), m3.get(2), 0,
-        m3.get(3), m3.get(4), m3.get(5), 0,
-        m3.get(6), m3.get(7), m3.get(8), 0,
-        0, 0, 0, 1,
-    ])
-})
+        expect(m4.toArray()).toStrictEqual([
+            m3.get(0), m3.get(1), m3.get(2), 0,
+            m3.get(3), m3.get(4), m3.get(5), 0,
+            m3.get(6), m3.get(7), m3.get(8), 0,
+            0, 0, 0, 1,
+        ])
+    })
 
-test('from vector3', () => {
-    const v3 = new Vector3(1, 2, 3)
+    test('vector3', () => {
+        const v3 = new Vector3(1, 2, 3)
 
-    const m4 = Matrix4.fromVector3(v3)
+        const m4 = Matrix4.fromVector3(v3)
 
-    expect(m4.toArray()).toStrictEqual([
-        1, 0, 0, v3.x,
-        0, 1, 0, v3.y,
-        0, 0, 1, v3.z,
-        0, 0, 0, 1,
-    ])
+        expect(m4.toArray()).toStrictEqual([
+            1, 0, 0, v3.x,
+            0, 1, 0, v3.y,
+            0, 0, 1, v3.z,
+            0, 0, 0, 1,
+        ])
+    })
 })
 
 test('invert', () => {
