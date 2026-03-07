5d7e2c15acdafdc9fc807c7d698c08fc7f1ed502
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-03-07 15:59:12 +0200

    start camera

--- test/unit/math/transform_matrix.spec.js
+++ test/unit/math/transform_matrix.spec.js
@@ -4,6 +4,9 @@ import Vertex from "../../../src/render/Vertex.js";
 import Vector4 from "../../../src/math/Vector4.js";
 import Canvas2DRenderer from "../../../src/render/Canvas2DRenderer.js";
 import Vector3 from "../../../src/math/Vector3.js";
+import Transform from "../../../src/object/Transform.js";
+import Matrix3 from "../../../src/math/Matrix3.js";
+import Matrix4 from "../../../src/math/Matrix4.js";
 
 describe('Transform matrix', () => {
     describe('Projection', () => {
@@ -17,13 +20,29 @@ describe('Transform matrix', () => {
             ({ vertex, matrixArguments, expected }) => {
                 const { canvasWidth, canvasHeight, viewportWidth, viewportHeight, viewportDistance } = matrixArguments
 
-                const m = RendererUtils.calculateProjectionAndMappingMatrix(canvasWidth, canvasHeight, viewportWidth, viewportHeight, viewportDistance)
+                const m4 = RendererUtils.calculateProjectionAndMappingMatrix(canvasWidth, canvasHeight, viewportWidth, viewportHeight, viewportDistance)
 
-                const projectedVertex = Canvas2DRenderer.projectVertex(vertex, m)
+                const projectedVertex = Canvas2DRenderer.projectVertex(vertex, m4)
 
                 for (let i = 0; i < projectedVertex.length; i++) {
                     expect(projectedVertex[i]).toBe(expected[i])
                 }
+            })
+    })
+
+    describe('Camera', () => {
+        test.each([
+            {
+                transform: new Transform(
+                    new Vector3(1, 1, 1),
+                    new Matrix3([]),
+                    1
+                ),
+                expected: new Matrix4([])
+            },
+        ])('project vertex ($vertex.position) using projection and mapping matrix ($expected)',
+            ({ transform, expected }) => {
+                const m4 = RendererUtils.calculateCameraMatrix(transform)
             })
     })
 })
